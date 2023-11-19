c2cad9e00acc15d8f43d260cf5a66f0179c60762
VallenDraa <jestinevallendradwiputra@gmail.com>
2023-11-20 03:18:29 +0700

    test: setup mock server

--- jest.setup.js
+++ jest.setup.js
@@ -1,2 +1,10 @@
 // Learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+// For ensuring that we can use fetch in testing evironment
+import 'whatwg-fetch';
+import { server } from '@/mocks/server';
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
