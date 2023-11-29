ed62c3e938324c49ec67a32d996f1c25515ca69d
VallenDraa <jestinevallendradwiputra@gmail.com>
2023-11-29 12:15:44 +0700

    chore: remove cancelled feature files

--- src/store/benang/benang.types.ts
+++
@@ -1,9 +0,0 @@
-import z from 'zod';
-
-export const benangSchema = z.object({
-  id: z.string(),
-  quantity: z.number(),
-  color: z.string(),
-});
-
-export const benangSchemaWithoutId = benangSchema.omit({ id: true });
