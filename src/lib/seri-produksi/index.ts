bf49a060daaa0ee38b7c321e19bdb8cf5dee5d41
VallenDraa <jestinevallendradwiputra@gmail.com>
2023-12-27 12:08:49 +0700

    chore: remove unused code and refactor code structure

--- src/lib/seri-produksi/index.ts
+++ src/lib/seri-produksi/index.ts
@@ -8,23 +8,3 @@ export async function seriProduksiExists(nomorSeri: number) {
 
   return seriProduksiExists;
 }
-
-export async function createSeriProduksi(nomorSeri: number, nama?: string) {
-  const exists = await seriProduksiExists(nomorSeri);
-
-  if (!exists) {
-    throw new Error('Nomor seri produksi yang ingin dibuat sudah digunakan!');
-  }
-
-  const newSeriProduksi = await prisma.seriProduksi.create({
-    data: { nomorSeri, nama },
-  });
-
-  return newSeriProduksi;
-}
-
-export async function deleteSeriProduksi(nomorSeri: number[]) {
-  await prisma.seriProduksi.deleteMany({
-    where: { nomorSeri: { in: nomorSeri } },
-  });
-}
