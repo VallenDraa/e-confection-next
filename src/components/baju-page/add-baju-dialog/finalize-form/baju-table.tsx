941c54d6a9c092ef16bd3103307440aba173edbd
VallenDraa <jestinevallendradwiputra@gmail.com>
2024-01-02 08:59:14 +0700

    feat: add baju table data sorting

--- src/components/baju-page/add-baju-dialog/finalize-form/baju-table.tsx
+++ src/components/baju-page/add-baju-dialog/finalize-form/baju-table.tsx
@@ -7,10 +7,11 @@ import {
   TableContainer,
   TableHead,
   TableRow,
+  TableSortLabel,
   TableFooter,
 } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
-import { Merek, Size } from '@prisma/client';
+import { Baju, Merek, Size } from '@prisma/client';
 import { ConfirmDeleteDialog } from '@/components/ui/confirm-delete-dialog';
 import { grey } from '@mui/material/colors';
 import { NewBaju } from '@/schema/baju.schema';
@@ -29,8 +30,10 @@ type BajuTableProps = {
   onBajuDelete?: (id: string) => void;
 };
 
+type BajuColumnIds = keyof NewBaju | 'hapus' | 'no';
+
 type BajuColumn = {
-  id: keyof NewBaju | 'hapus' | 'no';
+  id: BajuColumnIds;
   label:
     | 'No'
     | 'Merek'
@@ -108,6 +111,63 @@ export function BajuTable(props: BajuTableProps) {
     previewKaryawanList,
   } = props;
 
+  const [order, setOrder] = React.useState<'asc' | 'desc'>('asc');
+  const [orderBy, setOrderBy] = React.useState<keyof NewBaju>('merekId');
+
+  function handleSort(
+    event: React.MouseEvent<HTMLSpanElement, MouseEvent>,
+    property: keyof NewBaju,
+  ) {
+    setOrder(prev => (prev === 'asc' ? 'desc' : 'asc'));
+    setOrderBy(property);
+  }
+
+  const getRowValue = React.useCallback(
+    (baju: NewBaju, columnKey: keyof NewBaju) => {
+      let value;
+
+      switch (true) {
+        case columnKey === 'merekId':
+          value =
+            merekList.find(merek => merek.id === baju.merekId)?.nama ?? 'N/A';
+          break;
+
+        case columnKey === 'sizeId':
+          value = sizeList.find(size => size.id === baju.sizeId)?.nama ?? 'N/A';
+          break;
+
+        case columnKey === 'karyawanId':
+          value =
+            previewKaryawanList.find(
+              karyawan => karyawan.id === baju.karyawanId,
+            )?.nama ?? 'N/A';
+          break;
+
+        default:
+          value = baju[columnKey];
+          break;
+      }
+
+      return value;
+    },
+    [merekList, previewKaryawanList, sizeList],
+  );
+
+  const sortedBajuList = React.useMemo(() => {
+    return bajuList.sort((a, b) => {
+      const valueA = getRowValue(a, orderBy);
+      const valueB = getRowValue(b, orderBy);
+
+      if (valueA < valueB) {
+        return order === 'desc' ? -1 : 1;
+      } else if (valueA > valueB) {
+        return order === 'desc' ? 1 : -1;
+      } else {
+        return 0;
+      }
+    });
+  }, [getRowValue, bajuList, orderBy, order]);
+
   return (
     <TableContainer
       sx={{ backgroundColor: grey[200], maxHeight: 320, width: '100%' }}
@@ -126,14 +186,24 @@ export function BajuTable(props: BajuTableProps) {
                   align={column.align}
                   style={{ minWidth: column.minWidth }}
                 >
-                  {column.label}
+                  {column.id === 'no' || column.id === 'hapus' ? (
+                    column.label
+                  ) : (
+                    <TableSortLabel
+                      active={orderBy === column.id}
+                      direction={orderBy === column.id ? order : 'asc'}
+                      onClick={e => handleSort(e, column.id as keyof NewBaju)}
+                    >
+                      {column.label}
+                    </TableSortLabel>
+                  )}
                 </TableCell>
               );
             })}
           </TableRow>
         </TableHead>
         <TableBody>
-          {bajuList.map((baju, i) => {
+          {sortedBajuList.map((baju, i) => {
             return (
               <TableRow hover role="checkbox" tabIndex={-1} key={i}>
                 {columns.map(column => {
@@ -162,36 +232,8 @@ export function BajuTable(props: BajuTableProps) {
                       </TableCell>
                     );
                   } else {
-                    let value;
-
-                    switch (true) {
-                      case column.id === 'merekId':
-                        value =
-                          merekList.find(merek => merek.id === baju.merekId)
-                            ?.nama ?? 'N/A';
-                        break;
-
-                      case column.id === 'sizeId':
-                        value =
-                          sizeList.find(size => size.id === baju.sizeId)
-                            ?.nama ?? 'N/A';
-                        break;
-
-                      case column.id === 'karyawanId':
-                        value =
-                          previewKaryawanList.find(
-                            karyawan => karyawan.id === baju.karyawanId,
-                          )?.nama ?? 'N/A';
-                        break;
-
-                      case column.id === 'no':
-                        value = i + 1;
-                        break;
-
-                      default:
-                        value = baju[column.id];
-                        break;
-                    }
+                    const value =
+                      column.id === 'no' ? i + 1 : getRowValue(baju, column.id);
 
                     return (
                       <TableCell
